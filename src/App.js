c93d33081397c651dfd888f40e0da1899d9e9001
hugo-pb <h.palomerabecerra@gmail.com>
2022-01-04 13:52:28 -0700

    added bpptstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>hello world</h1>{" "}
+      <div className="container">
+        <h1>Hweather App</h1>
+        
+        <footer>
+          <p>
+            App created by{" "}
+            <a
+              href="https://github.com/hugo-pb"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Hugo-Palomera
+            </a>
+            , open source{" "}
+            <a
+              href="https://github.com/hugo-pb/React-Weather-App"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              here{" "}
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
