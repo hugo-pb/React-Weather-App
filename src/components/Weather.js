e8f0f672267233e50347d1734ade9b9b231ea2e7
hugo-pb <h.palomerabecerra@gmail.com>
2022-01-11 08:16:32 -0700

    added form to loading

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,7 +6,7 @@ import WeatherForcast from "./WeatherForcast";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
-const [city, setCity] =useState(props.defaultCity)
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(r) {
     setWeatherData({
       ready: true,
@@ -16,27 +16,24 @@ const [city, setCity] =useState(props.defaultCity)
       city: r.data.name,
       description: r.data.weather[0].description,
       date: new Date(r.data.dt * 1000),
-        feels: r.data.main.feels_like,
+      feels: r.data.main.feels_like,
       icon: r.data.weather[0].icon,
       lon: r.data.coord.lon,
-lat: r.data.coord.lat,
+      lat: r.data.coord.lat,
     });
-    }
-   function search(){
-        const apiKey = "c69faf9d2df6cf97f08634b08792a39d";
+  }
+  function search() {
+    const apiKey = "c69faf9d2df6cf97f08634b08792a39d";
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(handleResponse);
-   }
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault()
-       search()
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    search();
+  }
   if (weatherData.ready) {
-     
-    
-      
-      return (
+    return (
       <div className="Weather">
         <form onSubmit={handleSubmit}>
           <div className="row">
@@ -45,8 +42,8 @@ lat: r.data.coord.lat,
                 type="search"
                 placeholder="Search a City..."
                 className="form-control"
-                              autoFocus="on"
-                              onChange={e => setCity(e.target.value)}
+                autoFocus="on"
+                onChange={(e) => setCity(e.target.value)}
               />
             </div>
             <div className="col-3">
@@ -57,14 +54,38 @@ lat: r.data.coord.lat,
               />
             </div>
           </div>
-            </form>
-            <WeatherInfo data={weatherData} />
-          <WeatherForcast lon={weatherData.lon} lat= {weatherData.lat} />
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForcast lon={weatherData.lon} lat={weatherData.lat} />
       </div>
     );
-  }else {
-      search();
+  } else {
+    search();
 
-    return <p> loading.. </p>;
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search a City..."
+                className="form-control"
+                autoFocus="on"
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <p> loading.. </p>
+      </div>
+    );
   }
 }
